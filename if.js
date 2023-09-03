e9bdfbf3548c665aec09b402aa2686909230b663
Julieta <109936071+Julis19@users.noreply.github.com>
2023-09-03 11:02:31 -0300

    Add files via upload

---
+++ if.js
@@ -0,0 +1,21 @@
+
+var precioBaseHelado = 150;
+var precioTopping = 0;
+var precioFinal=0;
+
+    if(topping) {
+        topping == "oreo"
+        precioTopping = 10;
+    else if topping == "kitkat"
+        precioTopping = 15;
+    else if topping == "kinder"
+        precioTopping = 25;}
+    
+    else{
+        precioTopping = 0
+        console.log("No tenemos este topping")}
+    
+    precioFinal = precioBaseHelado + precioTopping
+    
+    console.log(""El helado cuesta $" + precioFinal”)
+    
